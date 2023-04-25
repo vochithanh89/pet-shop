990d3fb30845f395d6b0a1bc50e830caf98f3e75
vochithanh89 <thanh.vo@beready.academy>
2023-04-25 11:57:38 +0700

    update feature login for admin

--- data/api.js
+++ data/api.js
@@ -1,16 +1,26 @@
-const pageSize = 10;
+const products = JSON.parse(localStorage.getItem('products'));
+const categories = JSON.parse(localStorage.getItem('categories'));
+const users = JSON.parse(localStorage.getItem('users'));
+const roles = JSON.parse(localStorage.getItem('roles'));
+
+const pageSize = 5;
 
 const getProducts = ({
+    categoryId = 0,
     page = 1,
-    category = 0,
-    sort = 0
 }) => {
-    let result = products.filter((product) => {
-        return product.categoriesId.includes(category);
-    });
-    result = products.slice(pageSize * page - 1, pageSize * page + 10);
-    // 
-    return result.sort();
+    let result = products;
+    if (categoryId !== 0) {
+        result = result.filter((product) => {
+            return product.categoryId === categoryId;
+        });
+    }
+    result = result.slice(pageSize * (page - 1), pageSize * page);
+    return result;
+}
+
+const getCategories = () => {
+    return categories;
 }
 
 const getProduct = (id) => {
@@ -18,5 +28,25 @@ const getProduct = (id) => {
 }
 
 const getCategory = (id) => {
-    return products.find((product) => product.id === id);
+    return categories.find((category) => category.id === id);
+}
+
+const getUsers = () => {
+    return users;
+}
+
+const getRoles = () => {
+    return roles;
+}
+
+const authentication = (userName, password) => {
+    const user = users.find(user => {
+        return user.name === userName && user.password == password;
+    })
+
+    return {
+        isSuccess: !!user,
+        userId: user ? user.id : null,
+        roles: user ? user.roleId : null,
+    }
 }
--- data/data.js
+++ data/data.js
@@ -1,19 +1,204 @@
-// const products = [{
-//     id,
-//     name,
-//     description,
-//     thunail,
-//     images,
-//     categoryId,
-// }]
+(function () {
+    const products = [
+        {
+            id: 1,
+            name: 'Onesie dog pj',
+            price: 27,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/1.jpg',
+            images: [
+                '../../assets/images/products/1.jpg',
+                '../../assets/images/products/1_2.jpg',
+                '../../assets/images/products/1_3.jpg',
+                '../../assets/images/products/1_4.jpg',
+            ],
+            categoryId: 1,
+        },
+        {
+            id: 2,
+            name: 'Warm raincoat',
+            price: 58,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/1.jpg',
+            images: [
+                '../../assets/images/products/2.jpg',
+                '../../assets/images/products/2_2.jpg',
+                '../../assets/images/products/2_3.jpg',
+            ],
+            categoryId: 1,
+        },
+        {
+            id: 3,
+            name: 'Dog jersey',
+            price: 100,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/3.jpg',
+            images: [
+                '../../assets/images/products/3.jpg',
+                '../../assets/images/products/3_2.jpg',
+                '../../assets/images/products/3_3.jpg',
+            ],
+            categoryId: 1,
+        },
+        {
+            id: 4,
+            name: 'Dog pj',
+            price: 27,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/4.jpg',
+            images: [
+                '../../assets/images/products/4.jpg',
+                '../../assets/images/products/4_2.jpg',
+                '../../assets/images/products/4_3.jpg',
+            ],
+            categoryId: 1,
+        },
+        {
+            id: 5,
+            name: 'Mouse bell',
+            price: 58,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/5.jpg',
+            images: [
+                '../../assets/images/products/5.jpg',
+                '../../assets/images/products/5_2.jpg',
+                '../../assets/images/products/5_3.jpg',
+            ],
+            categoryId: 2,
+        },
+        {
+            id: 6,
+            name: 'Toy scratcher',
+            price: 48,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/6.jpg',
+            images: [
+                '../../assets/images/products/6.jpg',
+                '../../assets/images/products/6_2.jpg',
+                '../../assets/images/products/6_3.jpg',
+            ],
+            categoryId: 2,
+        },
+        {
+            id: 7,
+            name: 'Cat play house',
+            price: 200,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/7.jpg',
+            images: [
+                '../../assets/images/products/7.jpg',
+                '../../assets/images/products/7_2.jpg',
+                '../../assets/images/products/7_3.jpg',
+            ],
+            categoryId: 2,
+        },
+        {
+            id: 8,
+            name: 'Monthly crate',
+            price: 89,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/8.jpg',
+            images: [
+                '../../assets/images/products/8.jpg',
+                '../../assets/images/products/8_2.jpg',
+                '../../assets/images/products/8_3.jpg',
+            ],
+            categoryId: 3,
+        },
+        {
+            id: 9,
+            name: 'Toy ball shaped',
+            price: 25,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/9.jpg',
+            images: [
+                '../../assets/images/products/9.jpg',
+                '../../assets/images/products/9_2.jpg',
+                '../../assets/images/products/9_3.jpg',
+            ],
+            categoryId: 4,
+        },
+        {
+            id: 10,
+            name: 'Chew toy',
+            price: 28,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam.',
+            thunail: '../../assets/images/products/10.jpg',
+            images: [
+                '../../assets/images/products/10.jpg',
+                '../../assets/images/products/10_2.jpg',
+                '../../assets/images/products/10_3.jpg',
+            ],
+            categoryId: 4,
+        },
+    ];
+    
+    const categories = [
+        {
+            id: 1,
+            name: 'Dogs',
+            description: ''
+        }, {
+            id: 2,
+            name: 'Cats',
+            description: ''
+        },
+        {
+            id: 3,
+            name: 'Clothes',
+            description: ''
+        },
+        {
+            id: 4,
+            name: 'Toys',
+            description: ''
+        },
+    ];
 
-// const categories = [
-//     id,
-//     name,
-//     description
-// ]
+    const roles = [
+        {
+            id: 1,
+            name: 'Admin',
+        },
+        {
+            id: 2,
+            name: 'Content manager',
+        },
+        {
+            id: 3,
+            name: 'Customer',
+        },
+    ];
 
-// const sorts = [
-//     id,
-//     name
-// ]
+    const users = [
+        {
+            id: 1,
+            name: 'thanh',
+            password: '123456',
+            roleId: 1,
+        },
+    ];
+
+    const isProductsInLS = !!localStorage.getItem('products');
+    const isCategoriesInLS = !!localStorage.getItem('categories');
+    const isRolesInLS = !!localStorage.getItem('roles');
+    const isUsersInLS = !!localStorage.getItem('users');
+
+    if (!isProductsInLS) {
+        localStorage.setItem('products', JSON.stringify(products));
+    }
+
+    if (!isCategoriesInLS) {
+        localStorage.setItem('categories', JSON.stringify(categories));
+    }
+
+    if (!isRolesInLS) {
+        localStorage.setItem('roles', JSON.stringify(roles));
+    }
+
+    if (!isUsersInLS) {
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+
+
+})();
